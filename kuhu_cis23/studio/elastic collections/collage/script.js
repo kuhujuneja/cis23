ec053979009f50b4140ac6153665b2b3b87a4c9a
kuhujuneja <junek064@newschool.edu>
2023-04-06 00:53:31 -0400

    js filter all done

--- kuhu_cis23/studio/elastic collections/collage/script.js
+++ kuhu_cis23/studio/elastic collections/collage/script.js
@@ -1,35 +1,21 @@
 
+const buttons = document.querySelectorAll(".button");
 
-const gifContainers = document.querySelectorAll('.gif-container');
-
-gifContainers.forEach(gifContainer => {
-  const gif = gifContainer.querySelector('.gif');
-  const overlay = gifContainer.querySelector('.overlay');
-  const overlayContent = gifContainer.querySelector('.overlay-content');
-
-  gifContainer.addEventListener('click', () => {
-    gif.style.transform = 'scale(1.2)';
-    overlayContent.style.opacity = '1';
-    overlayContent.style.visibility = 'visible';
-  });
-
-  overlay.addEventListener('click', event => {
-    event.stopPropagation();
-    gif.style.transform = '';
-    overlayContent.style.opacity = '';
-    overlayContent.style.visibility = '';
-  });
-});
-function toggleButton(button) {
-  // remove selected class from all buttons
-  $(".button").removeClass("selected");
-  // add selected class to clicked button
-  $(button).addClass("selected");
-}
-
-$(document).ready(function() {
-  // call toggleButton function when a button is clicked
-  $(".button").click(function() {
-    toggleButton(this);
-  });
+buttons.forEach(function(button) {
+  button.addEventListener('click', function(event) {
+    
+    const clickedButtonValue = event.target.getAttribute('data-value');
+    let itemsToHide = document.querySelectorAll(`.gif-container:not(.${clickedButtonValue})`);
+    let itemsToShow = document.querySelectorAll(`.gif-container.${clickedButtonValue}`);
+    console.log(itemsToHide)
+   
+    itemsToHide.forEach(function(el) {
+      el.classList.add('hide');
+      el.classList.remove('show');
+    });
+    itemsToShow.forEach(function(el) {
+      el.classList.remove('hide');
+      el.classList.add('show'); 
+    });
+  })
 });
